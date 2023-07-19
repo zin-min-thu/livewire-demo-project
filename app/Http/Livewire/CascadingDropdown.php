8bd5accfc3a5814a0697342fa2691eb62ed5e535
KoZin <cuzinminthu@gmail.com>
2023-07-19 21:26:14 +0630

    live wire select box action

--- app/Http/Livewire/CascadingDropdown.php
+++ app/Http/Livewire/CascadingDropdown.php
@@ -3,11 +3,28 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Continent;
+use App\Models\Country;
 
 class CascadingDropdown extends Component
 {
+    public $continents = [], $countries = [];
+    public $selectedContinent, $selectedCountry;
+
+    public function mount()
+    {
+        $this->continents = Continent::orderBy('name', 'asc')->get();
+    }
+
     public function render()
     {
         return view('livewire.cascading-dropdown');
+    }
+
+    public function changeContinent()
+    {
+        if($this->selectedContinent !== '-1') {
+            $this->countries = Country::where('continent_id', $this->selectedContinent)->orderBy('name', 'asc')->get();
+        }
     }
 }
