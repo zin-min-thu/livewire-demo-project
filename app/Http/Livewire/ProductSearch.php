00d6171020f4ad860ce4c7d039beb83bd3fef8b0
KoZin <cuzinminthu@gmail.com>
2023-07-20 10:59:12 +0630

    product search with pagination

--- app/Http/Livewire/ProductSearch.php
+++ app/Http/Livewire/ProductSearch.php
@@ -3,11 +3,35 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\Product;
 
 class ProductSearch extends Component
 {
+    use WithPagination;
+
+    public $search = '';
+
+    protected $queryString = ['search'];
+
     public function render()
     {
-        return view('livewire.product-search');
+        $query = Product::query();
+
+        if($this->search) {
+            $query->where('title', 'like', "%{$this->search}%")
+                ->orWhere('description', 'like', "%{$this->search}%");
+        }
+
+        return view('livewire.product-search', [
+            'products' => $query->paginate(10)
+        ]);
+    }
+
+    public function updated($property)
+    {
+        if($property == 'search') {
+            $this->resetPage();
+        }
     }
 }
--- resources/views/livewire/product-search.blade.php
+++ resources/views/livewire/product-search.blade.php
@@ -1,3 +1,26 @@
-<div>
-    <h1>Product page</h1>
+<div class="container mx-auto py-16 px-8">
+    <div class="mb-4">
+        <input type="text" wire:model.lazy="search" placeholder="search...">
+    </div>
+    <table class="table-auto w-full mb-4">
+        <thead>
+            <tr>
+                <th class="py-2 px-3 bg-gray-100 border-b-2 text-left">ID</th>
+                <th class="py-2 px-3 bg-gray-100 border-b-2 text-left">Image</th>
+                <th class="py-2 px-3 bg-gray-100 border-b-2 text-left">Title</th>
+                <th class="py-2 px-3 bg-gray-100 border-b-2 text-left">Price</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($products as $product)
+                <tr>
+                    <td class="py-2 px-3 border-b">{{ $product->id }}</td>
+                    <td class="py-2 px-3 border-b"><img class="w-16" src="{{ $product->image }}" alt="{{ $product->title }}"></td>
+                    <td class="py-2 px-3 border-b">{{ $product->title }}</td>
+                    <td class="py-2 px-3 border-b">{{ $product->price }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    {{ $products->links() }}
 </div>
